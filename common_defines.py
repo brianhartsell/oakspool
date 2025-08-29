f8299af34c8acd35a0bacb182f57b3412c7d526b
brianhartsell <83544376+brianhartsell@users.noreply.github.com>
2025-08-29 16:33:39 -0500

    Create common_defines.py

---
+++ common_defines.py
@@ -0,0 +1,5 @@
+SEASONS = [
+    {"year": 2023, "open": datetime.date(2023, 5, 26), "close": datetime.date(2023, 9, 3), "rate": 5.60},
+    {"year": 2024, "open": datetime.date(2024, 5, 24), "close": datetime.date(2024, 9, 1), "rate": 6.15},
+    {"year": 2025, "open": datetime.date(2025, 5, 23), "close": datetime.date(2025, 8, 31), "rate": 6.70},
+]
